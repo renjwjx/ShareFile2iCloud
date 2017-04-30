285d7fd4394498fa3561094e64182a214e23e6de
renjwjx <renjwjx@gmail.com>
2017-04-30 21:17:23 +0800

    share file to iCloud.

--- sharing-file/ViewController.m
+++ sharing-file/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) NSURL* filePath;
 @end
 
 @implementation ViewController
@@ -17,12 +18,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSArray* array = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+    NSString* pathStr = [NSString stringWithFormat:@"%@%@",(NSString*)array[0], @"app.log"];
+    NSURL*url = [NSURL URLWithString:pathStr];
+    self.filePath = url;
+    NSString* fileStr = @"file test content";
+    [fileStr writeToURL:url atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)shareFile:(id)sender {
+    UIActivityViewController* activeVC = [[UIActivityViewController alloc] initWithActivityItems:@[self.filePath] applicationActivities:nil];
+    
+    [self presentViewController:activeVC animated:YES completion:^{
+        NSLog(@"completion");
+    }];
 }
 
 
